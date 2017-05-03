eab76fb4252c644898d906f5399ee60409867097
ysknsn <ysknsn@gmail.com>
2017-05-03 17:02:20 +0900

    set port

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +9,8 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port)
+
+
+    # app.run()
