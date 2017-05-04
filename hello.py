c3e13b99943b9a8b08bbc2ddba47307b77728b5e
ysknsn <ysknsn@gmail.com>
2017-05-04 12:42:28 +0900

    add html files

--- hello.py
+++ hello.py
@@ -1,16 +1,34 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
-def home():
-    return 'Hello world!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0',port=port)
-
-
-    # app.run()
